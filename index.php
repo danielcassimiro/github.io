4362cf1ef91162472df26f4735aae1f53847039f
danielcassimiro <daniel.cassimiro@outlook.com>
2016-07-06 10:15:31 -0300

    First Class

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+    <head> 
+    <title> Curso Basico PHP RAS - UFABC</title>
+    
+    </head>
+    <body bgcolor ='#EEE9E9' height="90%">
+   
+        <center> <h1><b> Curso Basico PHP </b> </h1></center>
+        <ol>
+             <li> Introducao a Web</li>
+             <li> Diagramacao Basica</li>
+             <li> Listas</li>
+             <li> Imagens </li>
+             <li> Tableas </li>
+             <li> Forms</li>
+             <li><a href="http://www.w3schools.com/default.asp"> Curso W3Schools </a></li>
+        </ol>
+        
+    </body>
+    
+    <foot height="10%"> By Daniel Cassimiro </foot>
+</html>
+
+
+<!-- 
+<img src="imagem.jpg" width="50" height="50" alt="Descrição da imagem" />
+
+
+Read more: http://www.linhadecodigo.com.br/artigo/81/html-basico.aspx#ixzz42v38Bu5o-->
